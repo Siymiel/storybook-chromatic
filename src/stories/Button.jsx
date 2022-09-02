1010542188dcbd19b6ff1cdcd61aff2b2b96ab8e
Samuel Kinuthia <51239970+Siymiel@users.noreply.github.com>
2022-09-03 00:05:11 +0300

    shifted text

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
-// "chromatic": "npx chromatic --project-token=efadc92e3034"
 
+// "chromatic": "npx chromatic --project-token=efadc92e3034"
 /**
  * Primary UI component for user interaction
  */
